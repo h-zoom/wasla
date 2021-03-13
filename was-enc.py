35f151478843d33eb4f93f121cd2e123ee0c4e71
Hazem <70172866+h-zoom@users.noreply.github.com>
2021-03-14 01:48:36 +0200

    Add files via upload

---
+++ was-enc.py
@@ -0,0 +1,14 @@
+import requests,json,random,string,termcolor,time,pyfiglet
+from bs4 import BeautifulSoup
+# -----------------------------------
+import os,sys,time
+def jalan(z):
+ for e in z + '\n':
+  sys.stdout.write(e)
+  sys.stdout.flush()
+  time.sleep(00000.04)
+  input
+jalan("  \033[1;32m الاسكربت في الصيانه قريبا هيتم التشغيل")
+jalan('\033[1;39m HAZEM ELMARSHAL')
+
+
